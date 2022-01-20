f0a2a1bc963bf62bbb1a842d1b6c6e34cd7e005a
PedroMartinezGC <97804671+PedroMartinezGC@users.noreply.github.com>
2022-01-20 19:41:19 +0100

    styles-1.1
    About page modification.

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -13,7 +13,7 @@ export class AboutComponent implements OnInit {
 
   constructor() {
   	this.title = "PEDRO MARTÍNEZ-GRANDE CICUÉNDEZ";
-  	this.subtitle = "Ingeniero Mecánico y Programador";
+  	this.subtitle = "Mechanical engineer and programmer";
   	this.email = "pedromartinezgcicuendez@gmail.com";
   }
 
