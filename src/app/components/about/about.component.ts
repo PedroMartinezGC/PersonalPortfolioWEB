65f0be9ac79e0dd3d8291946dd4bd21f94a8b6a2
PedroMartinezGC <97804671+PedroMartinezGC@users.noreply.github.com>
2022-01-20 16:59:04 +0100

    First commit
    Start point to create the app.

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+	public title: string;
+	public subtitle: string;
+	public email: string;
+
+
+  constructor() {
+  	this.title = "PEDRO MARTÍNEZ-GRANDE CICUÉNDEZ";
+  	this.subtitle = "Ingeniero Mecánico y Programador";
+  	this.email = "pedromartinezgcicuendez@gmail.com";
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/services/global.ts
@@ -0,0 +1,8 @@
+/* Este fichero es para meter los archivos globales, es decir, cosas que voy a usar de forma
+general, y de esta forma no tengo que estar luego escribiéndolas constantemente en todos los 
+demás ficheros. */
+
+export var Global = {
+	url: 'http://localhost:3700/api/'
+};
+
